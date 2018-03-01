94f7f8070922fc4c530c4d3ecafe79fd94d399a7
kesava <psyku@nottingham.ac.uk>
2018-03-01 20:01:55 +0000

    Created class for on click action and create world

---
+++ Assets/Code/ClickBlock.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickBlock : MonoBehaviour
+{
+
+	// This function starts when the mouse cursor is over the GameObject
+	void OnMouseOver()
+	{
+		// If left mouse press
+		if (Input.GetMouseButtonDown(0))
+		{
+			// Display this text in the Console
+			Debug.Log("Left button has been clicked");
+		}
+
+		// If right mouse press
+		if (Input.GetMouseButtonDown(1))
+		{
+			// Display a message in the Console tab
+			Debug.Log("Right button has been clicked");
+		}
+	}
+}
