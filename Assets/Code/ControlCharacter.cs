7eda9517f56cfd3fbc06f35cb528fd46e3bf6a8e
kesava <kesava@live.co.uk>
2018-03-11 23:13:10 +0000

    Added comments to ControlCharacter

--- Assets/Code/ControlCharacter.cs
+++ Assets/Code/ControlCharacter.cs
@@ -3,23 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ControlCharacter : MonoBehaviour {
-
+	//response of character to keyboard movements 
 	public float speed = 10.0f;
 	// Use this for initialization
 	void Start () {
+	//turn of cursor on screen and keeps within game window
 		Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//vertical movement of character
+		float vtranslation = Input.GetAxis ("Vertical") * speed;
+		//horizontal movement of character 
+		float htranslation = Input.GetAxis("Horizontal")*speed;
+		//keep movements smooth and keeps in time with update loop
+		//delta time is the time between last update and current update
+		vtranslation *= Time.deltaTime; 
+		htranslation *= Time.deltaTime;
 
-		float translation = Input.GetAxis ("Vertical") * speed;
-		float straffe = Input.GetAxis("Horizontal")*speed;
-		translation *= Time.deltaTime;
-		straffe *= Time.deltaTime;
-
-		transform.Translate (straffe, 0, translation);
-
+		transform.Translate (htranslation, 0, vtranslation);
+		//turns cursor back on if escape key is pressed
 		if (Input.GetKeyDown ("escape"))
 			Cursor.lockState = CursorLockMode.None; 
 		
