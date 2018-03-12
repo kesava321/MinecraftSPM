0a9ed9a2ac9f96f276207c8bbff11eb9a70f9d19
wenhao <psxww4@ad.nottingham.ac.uk>
2018-03-12 01:08:54 +0000

    chage UI 3.0

---
+++ Assets/Scribe/hotkey.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hotkey : MonoBehaviour {
+	
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.I)) {
+			
+					OpenItemMenu();
+			gameObject.UI - bag;
+				
+				}
+			}
+
+	public void OpenItemMenu()
+			{
+				if (!UI-bag)
+				{
+					Time.timeScale = 0;
+				}
+				else
+				{
+					Time.timeScale = 1;
+				}
+		         UI-bag = !UI-bag;
+		GameObject.SetActive(UI-bag);
+			}
+		}
