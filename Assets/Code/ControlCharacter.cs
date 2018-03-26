25289d168e0d9f681e2f9b18e1fa718351fa0430
Wayne Thrower <psywgt@nottingham.ac.uk>
2018-03-26 17:30:33 +0100

    Add jump and character controller to player

--- Assets/Code/ControlCharacter.cs
+++ Assets/Code/ControlCharacter.cs
@@ -3,29 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ControlCharacter : MonoBehaviour {
-	//response of character to keyboard movements 
-	public float speed = 10.0f;
-	// Use this for initialization
+	
+    //response of character to keyboard movements 
+	public float speed = 15.0f;
+
+    public float jumpForce = 20f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDir = Vector3.zero;
+	
+    // Use this for initialization
 	void Start () {
-	//turn of cursor on screen and keeps within game window
+	
+        //turn of cursor on screen and keeps within game window
 		Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//vertical movement of character
-		float vtranslation = Input.GetAxis ("Vertical") * speed;
-		//horizontal movement of character 
-		float htranslation = Input.GetAxis("Horizontal")*speed;
-		//keep movements smooth and keeps in time with update loop
+		
+        //vertical movement of character
+		//float vtranslation = Input.GetAxis ("Vertical") * speed;
+		
+        //horizontal movement of character 
+		//float htranslation = Input.GetAxis("Horizontal")*speed;
+		
+        //keep movements smooth and keeps in time with update loop
 		//delta time is the time between last update and current update
-		vtranslation *= Time.deltaTime; 
-		htranslation *= Time.deltaTime;
+		//vtranslation *= Time.deltaTime; 
+		//htranslation *= Time.deltaTime;
 
-		transform.Translate (htranslation, 0, vtranslation);
-		//turns cursor back on if escape key is pressed
+		//transform.Translate (htranslation, 0, vtranslation);
+		
+        //turns cursor back on if escape key is pressed
 		if (Input.GetKeyDown ("escape"))
-			Cursor.lockState = CursorLockMode.None; 
+			Cursor.lockState = CursorLockMode.None;
+
+        CharacterController controller = gameObject.GetComponent<CharacterController>();
+
+        if (controller.isGrounded)
+        {
+            moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+            moveDir = transform.TransformDirection(moveDir);
+
+            moveDir *= speed;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDir.y = jumpForce;
+
+            }
+
+        }
+
+        moveDir.y -= gravity * Time.deltaTime;
+
+        controller.Move(moveDir * Time.deltaTime);
 		
 	}
+
 }
