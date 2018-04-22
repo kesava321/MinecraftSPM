aae7159a2b3dd0c1064c51b9521b1360f6711e06
EFFY <895345817@qq.com>
2018-04-22 16:03:54 +0100

    blockSpawn

---
+++ Assets/Pickup/blockcreation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class blockcreation: MonoBehaviour{
+
+	public GameObject block;
+
+
+	void spawn()
+	{
+		Vector3 blockPos = new Vector3 (this.transform.position.x + Random.Range (-1.0f, 1.0f),
+			this.transform.position.y + Random.Range (0.0f, 2.0f),
+			this.transform.position.y + Random.Range (0.0f, 2.0f));
+		Instantiate (block, blockPos, Quaternion.identity);
+	}
+
+	void Start(){
+		spawn ();
+	}
+
+	void Update(){
+	}
+}
