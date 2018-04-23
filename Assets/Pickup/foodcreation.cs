a4177facd60e90a79348af11f5caf18ae08d6e7c
EFFY <psxbl3@nottingham.ac.uk>
2018-04-23 08:25:04 +0100

    new pickup method

---
+++ Assets/Pickup/foodcreation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class foodcreation: MonoBehaviour{
+
+	public GameObject block;
+	int blockNum = 8;
+
+	void spawn()
+	{
+		for(int i= 0; i<blockNum; i++)
+		{
+			Vector3 blockPos = new Vector3 (this.transform.position.x+Random.Range(-1.0f,1.0f),
+				this.transform.position.y+Random.Range(-1.0f,2.0f), 
+				this.transform.position.z+Random.Range(-1.0f,1.0f));
+			Instantiate (block, blockPos, Quaternion.identity);
+			
+		}
+
+
+	}
+		
+
+	void Start(){
+		spawn ();
+	}
+
+	void Update(){
+	}
+}
